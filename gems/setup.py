161adde7505703d1b102f9d10ab18161d0c0d514
abhishekcms611@gmail.com <abhishekcms611@gmail.com>
2024-11-28 05:26:57 +0000

    Add new SQL macros for data transformation and cleansing tasks

---
+++ gems/setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import setup, find_packages
+setup(
+    name = 'AlteryxSnowflake',
+    version = '0.0.1',
+    packages = ['AlteryxSnowflake'],
+    package_dir = {'AlteryxSnowflake': '.'},
+    description = '',
+    install_requires = [],
+)
+
